74330df3d431294be57bc9090ca284cbdf4eeddf
wouterrvdb <anon@ymous.com>
2018-05-01 21:49:27 +0200

    Update used CSV file; ignore first two project topics

--- src/custom_parser.py
+++ src/custom_parser.py
@@ -6,6 +6,7 @@ def parse(csv_path):
     counter = 0
     current_topic = ''
     component_list = []
+    parsing = False
     with open(csv_path, newline='') as csv_file:
         reader = csv.reader(csv_file, skipinitialspace=True)
         # Row is of the form "ID, Dependencies, Topic, Description, Min Time, Prob Time, Max Time, Time PM, Cost Materials"
@@ -17,6 +18,9 @@ def parse(csv_path):
             # Some rows are just topics, cfr. "chapters" of the project
             if row[0] is '':
                 current_topic = row[2]
+                # Ignore the first two project components
+                if current_topic == "Elektrische installatie (Cluster A)":
+                    parsing = True
                 continue
             # Make the dependencies list empty if there are no dependencies
             dependencies = []
@@ -25,11 +29,12 @@ def parse(csv_path):
                 dependencies = [string.strip() for string in row[1].split(',')]
             worker_times = [float(row[3]), float(row[4]), float(row[5])]
             project_manager_time = float(row[6])
-            component_list.append(ProjectComponent(component_id=str(row[0]),
-                                                   dependencies=dependencies,
-                                                   topic=current_topic,
-                                                   description=row[2],
-                                                   worker_times=worker_times,
-                                                   project_manager_time=project_manager_time,
-                                                   material_cost=0.0))  # TODO: Update costs
+            if parsing:
+                component_list.append(ProjectComponent(component_id=str(row[0]),
+                                                       dependencies=dependencies,
+                                                       topic=current_topic,
+                                                       description=row[2],
+                                                       worker_times=worker_times,
+                                                       project_manager_time=project_manager_time,
+                                                       material_cost=0.0))  # TODO: Update costs
     return component_list
